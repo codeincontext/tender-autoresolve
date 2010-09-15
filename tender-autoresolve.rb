55a730a430529829ad23cc296f4520bba10b1333
Adam Howard <adam.howard@audioboo.fm>
2010-09-15 17:38:46 +0100

    Add verbose mode

--- tender-autoresolve.rb
+++ tender-autoresolve.rb
@@ -6,17 +6,21 @@ require 'net/http'
 CFG = YAML.load_file("config.yml")
 @http = Net::HTTP.new(CFG['api_url'])
 
+def verb message
+  puts message if CFG['verbose']==true
+end
+
 def resolve(url)
   url.gsub!(/http:\/\/#{CFG['api_url']}/, '')
   
   request = Net::HTTP::Post.new(url, CFG['headers'])
   request.set_form_data(CFG['form_data'])
   response = @http.request(request)
-  puts "#{response.code} - #{url}"
+  verb "      Resolved (#{response.code})"
 end
 
 def scan_discussion(discussion)
-  false
+  verb "    Discussion #{discussion['number']}"
   if Time.parse(discussion['last_updated_at']) < CFG['time_period'].to_i.days.ago
     if CFG['quiet_resolve']
       resolve discussion['resolve_href']
@@ -24,10 +28,14 @@ def scan_discussion(discussion)
       resolve discussion['comments_href']
     end
     true
+  else
+    verb "      Too recent (no need to scan further)"
+    false
   end
 end
 
 def scan_page(state, page)
+  verb "  Page #{page}"
   request = Net::HTTP::Get.new("/#{CFG['project_path']}/discussions/#{state}?page=#{page}", CFG['headers'])
   response = @http.request(request)
 	parsed = JSON::parse response.body
@@ -41,7 +49,7 @@ def scan_page(state, page)
 end
 
 def scan_state(state)
-  puts "State: #{state}"
+  verb "State: #{state}"
   request = Net::HTTP::Get.new("/#{CFG['project_path']}/discussions/#{state}", CFG['headers'])
   response = @http.request(request)
   parsed = JSON::parse response.body
